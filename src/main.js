fd9c2636c13d14fca3cf879a5678209cad35cd17
焦品貴 <jpink0210@gmail.com>
2023-04-26 17:17:39 +0800

    [安裝] vue2-common

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './routes'
 import store from './store'
 
+import "common.styles/global.import.scss";
+
 new Vue({
   router,
   store,
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ function resolve (dir) {
     return path.join(__dirname, dir)
 }
 module.exports = {
+  transpileDependencies: [/node_modules[/\\\\]vue2-common[/\\\\]/],
   chainWebpack: (config)=>{
     config.resolve.alias
       .set('@$', resolve('src'))
@@ -12,6 +13,21 @@ module.exports = {
       .set('scss',resolve('src/scss'))
       .set('api',resolve('src/api'))
       .set('pages',resolve('src/pages'))
+      .set(
+        "common.components",
+        "@/../node_modules/vue2-common/src/components"
+      )
+      .set("common.styles", "@/../node_modules/vue2-common/src/scss")
+      .set('@vue2-common', '@/../node_modules/vue2-common/src')
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `
+          @import 'common.styles/global.variables.scss';
+        `
+      }
+    }
   },
   lintOnSave: false
 }
