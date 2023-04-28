9371a29bd0265cefe86d15928398960043e2a09b
焦品貴 <jpink0210@gmail.com>
2023-04-28 14:09:41 +0800

    [Page] Inputs 頁面建立

--- src/pages/inputs/index.vue
+++ src/pages/inputs/index.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
-    this is Inputs
+  <div class="inputs">
+    <div class="container">
+      <div class="title rounded bg-white p-4 text-primary h1">Vue2 Inputs</div>
+    </div>
+    <ContainerSidebar class="mt-4">
+      <template slot="main">
+        <div></div>
+      </template>
+      <template slot="sidebar">
+        <div class="forms-menu bg-white rounded">
+        </div>
+      </template>
+    </ContainerSidebar>
   </div>
 </template>
 
 <script>
+import ContainerSidebar from "components/shared/ContainerSidebar";
+
 export default {
   name: 'Inputs',
+  components: {
+    ContainerSidebar
+  },
   mounted: function () {
   },
   methods: {
@@ -14,3 +30,14 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.inputs {
+  .main {
+    min-height: 20px;
+  }
+  .forms-menu {
+    min-height: 100px;
+  }
+}
+</style>
