e4689a0b78bb22ff1dbf14fefddd3af985887f03
焦品貴 <jpink0210@gmail.com>
2023-08-10 15:54:40 +0800

    [Forms] make form RowComp.

---
+++ src/components/FormsRow.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="forms-row my-4 row">
+      <div class="col col-2">
+        <div class="font-weight-bold text-nowrap d-flex align-items-center h-100">
+          <slot name="title"></slot>
+        </div>
+      </div>
+      <slot></slot>
+    </div>
+</template>
+
+
+<script>
+  export default {
+    name: "FormsRow"
+  }
+</script>
+
+
+<style lang="scss" scoped>
+  .forms-row {
+  }
+
+</style>
